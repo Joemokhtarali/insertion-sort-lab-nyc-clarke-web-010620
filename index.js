819c1c500f43a368acd3669549b5b07fee6da4b1
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-07-15 13:58:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
+// function findMinAndRemove(array){
+//    return array.shift()
+// }
+
+// function selectionSort(arr){
+//     for(let i = 0; i < arr.length; i++){
+//         let lowest = i
+//         for(let j = i+1; j < arr.length; j++){
+//         if(arr[j] < arr[lowest]){
+//           lowest = j
+//         }
+//       }
+//         let temp = arr[i]
+//         arr[i] = arr[lowest] 
+//         arr[lowest] = temp 
+//       }
+//       return arr
+// }
+
+
 function findMinAndRemove(array){
-
-}
-
-function selectionSort(array){
-
-}
+    let currentMin = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++){
+      if(array[i] < currentMin){
+        currentMin = array[i]
+        minIndex = i
+      }
+    }
+    array.splice(minIndex, 1);
+    return currentMin;
+  }
+  
+  function selectionSort(array){
+    let sorted = []
+    let min;
+    while(array.length != 0){
+      min = findMinAndRemove(array)
+      sorted.push(min)
+    }
+    return sorted;
+  }
